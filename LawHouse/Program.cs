c35b97ba4d4d773e464262458ff45ea3553901b7
Dennie Christensen <44695505+Dennienajs@users.noreply.github.com>
2018-11-26 13:58:05 +0100

    Revert "Gui/overblik"

--- LawHouse/Program.cs
+++ LawHouse/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GUI
+namespace LawHouse
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new New_case());
+            Application.Run(new Form1());
         }
     }
 }
--- LawHouse/Properties/Settings.Designer.cs
+++ LawHouse/Properties/Settings.Designer.cs
@@ -8,29 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GUI.Properties {
-    
-    
+namespace LawHouse.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=den1.mssql8.gear.host;Persist Security Info=True;User ID=goodboysplus" +
-            "1v2;Password=Kr995B7kts!_")]
-        public string ConnectionStringDropdown {
-            get {
-                return ((string)(this["ConnectionStringDropdown"]));
             }
         }
     }
